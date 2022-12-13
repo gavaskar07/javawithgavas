6396487e68efe85df45dc9094e46070a024e27ff
kezia-t-george <118994397+kezia-t-george@users.noreply.github.com>
2022-12-13 13:08:47 +0530

    Collections Program added

---
+++ JAVA_Programs/Array_list/src/array_list/Array_list.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package array_list;
+import java.util.*;
+/**
+ *
+ * @author HP
+ */
+public class Array_list {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ArrayList<Integer> gavas_array=new ArrayList<Integer>();
+        gavas_array.add(1);
+        gavas_array.add(2);
+        Iterator itr=gavas_array.iterator();  
+while(itr.hasNext()){  
+System.out.println(itr.next());  
+}  
+
+    }
+    
+}
